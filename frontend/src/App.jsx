77a4b7224a8268217c4ef5bc6d2ff238fe80efa3
Laurent Dubois <laurent.dubois91710@gmail.com>
2025-05-17 02:44:52 +0200

    :sparkles: PROTECTED ROUTES :sparkles:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import FeedPage from "./pages/FeedPage"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-
+import ProtectedRoutes from "./components/ProtectedRoutes"
 
 
 function App() {
@@ -8,8 +8,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<FeedPage />} />
-        <Route path="/feed" element={<FeedPage />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<FeedPage />} />
+          <Route path="/feed" element={<FeedPage />} />
+        </Route>
       </Routes>
     </Router>
   )
